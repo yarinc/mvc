826b7d6bb93ca53ea9ba2e2e9e884cea662a3fa0
Yarinc <Yarinc@Yarin>
2015-09-17 01:14:37 +0300

    Fixing the input splitting from the user

--- MVC/src/view/CLI.java
+++ MVC/src/view/CLI.java
@@ -17,24 +17,48 @@ public class CLI implements Runnable {
 		this.out = out;
 		this.map = map;
 	}
-	public void start() throws IOException  { 
-		String line; 
-		while (!(line = in.readLine()).equals("exit")) { 
-			if(map.containsValue(line)) 
-				(map.get(line)).doCommand();
-			else
-				out.println("Command not Exists");
-		}
-		in.close();
-		out.close();
-	}
-	@Override
-	public void run() {
+	public void start() { 
+		String line;
+		String input;
+		String[] parameters;
 		try {
-			start();
+			while (!(line = in.readLine()).equals("exit")) {
+				input = extractCommand(line);
+				if(!(input.equals("Command not found"))) {
+					parameters = extratParameters(line, input);
+					(map.get(input)).doCommand(parameters);
+				}
+				else
+					out.println("Command not Exists");
+			}
+			in.close();
+			out.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-
+	private String[] extratParameters(String line, String input) {
+		String[] parameters;
+		parameters = (line.substring(input.length()+1)).split(" ");
+		return parameters;
+	}
+	@Override
+	public void run() {
+		start();
+	}
+	private String extractCommand(String input) {
+		String[] splited = input.split(" ");
+		int i;
+		String command = new String();
+		for(i=0;i<splited.length;i++) { 
+			command = command + splited[i];
+			if(map.containsKey(command)) { 
+				return command; 
+			}
+			else
+				command = command + " ";
+		}
+		return "Command not found"; 
+		
+	}
 }
