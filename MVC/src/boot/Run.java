6a46ff6f1f05a8e75f79fd3eab76f6f2107a69ed
Yarinc <Yarinc@Yarin>
2015-09-21 21:25:32 +0300

    Fix code

--- MVC/src/boot/Run.java
+++ MVC/src/boot/Run.java
@@ -1,19 +1,25 @@
 package boot;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
 import controller.MyController;
 import model.MyModel;
+import view.CLI;
 import view.MyView;
 
 public class Run {
 
 	public static void main(String[] args) {
+		CLI cli = new CLI(new BufferedReader(new InputStreamReader(System.in)),new PrintWriter(System.out));
 		MyModel model = new MyModel();
-		MyView view = new MyView();
+		MyView view = new MyView(cli);
+		cli.setView(view);
 		MyController controller = new MyController(model, view); 
 		model.SetController(controller);
 		view.SetController(controller);
 		view.start();
-
 	}
 
 }
